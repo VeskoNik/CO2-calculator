fe10b77935f33262c8f057aa71663ead673f416e
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-05-05 23:05:59 +0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { homeView } from "../views/home.js";
+import { resultView } from "../views/result.js";
+import { page,render } from "./lib.js";
+
+const root = document.querySelector('body');
+
+page(decorateContext);
+page('/index.html' , '/');
+page('/' , homeView);
+page('/result' , resultView);
+page.start();
+
+export function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    next();
+}
