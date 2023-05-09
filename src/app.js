705098f0e591befe1f8934f105642f909f31b6ad
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-05-09 12:50:22 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
-debugger
 import { homeView } from "../views/home.js";
 import { resultView } from "../views/result.js";
 import { page,render } from "./lib.js";
-debugger
 console.log('it works')
 const root = document.getElementById('root');
 
@@ -11,7 +9,7 @@ page('/CO2-calculator/' , '/');
 page('/' , homeView);
 page('/result' , resultView);
 page.start();
-debugger
+
 export function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, root);
     next();
