f0b2473723df2a57596a5c52dc69b0e0818b4dfd
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-05-09 12:12:47 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+debugger
 import { homeView } from "../views/home.js";
 import { resultView } from "../views/result.js";
 import { page,render } from "./lib.js";
+debugger
 console.log('it works')
 const root = document.getElementById('root');
 
@@ -9,7 +11,7 @@ page('/index.html' , '/');
 page('/' , homeView);
 page('/result' , resultView);
 page.start();
-
+debugger
 export function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, root);
     next();
