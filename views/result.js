596740f6f856c2b487c1a0f8131ee4d67f10b7c0
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-06-01 22:17:58 +0300

    Update result.js

--- views/result.js
+++ views/result.js
@@ -20,6 +20,6 @@ export function resultView(ctx) {
     ctx.render(loadingTemplate())
     setTimeout(() => {ctx.render(resultTemplate(result,onClick))},3000)
     function onClick() {
-        page.redirect('/')
+        page.redirect('CO2-calculator/')
     }
 }
